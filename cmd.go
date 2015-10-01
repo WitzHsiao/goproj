5ecaf8bc00b56f3280736beb2cb78fc1d53ed53e
Witz Hsiao <witz.hsiao@linecorp.com>
2015-10-01 19:30:08 +0800

    use "/usr/bin/env zsh" or "/usr/bin/env bash" instead login

--- cmd.go
+++ cmd.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"os/user"
 
 	"gopkg.in/yaml.v2"
 )
@@ -41,41 +40,28 @@ func here() {
 }
 
 func setEnv() {
-	// Get the current user.
-	me, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-
-	// Set an environment variable.
 	pwd, _ := os.Getwd()
-	os.Setenv("GOPATH", pwd)
 
-	// Transfer stdin, stdout, and stderr to the new process
-	// and also set target directory for the shell to start in.
-	pa := os.ProcAttr{
+	pa := &os.ProcAttr{
 		Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
 		Dir:   pwd,
+		Env:   append(os.Environ(), fmt.Sprintf("GOPATH=%s", pwd)),
 	}
 
-	// Start up a new shell.
-	// Note that we supply "login" twice.
-	// -fpl means "don't prompt for PW and pass through environment."
 	fmt.Print(">> Starting a new interactive shell and set GOPATH\n")
 
-	proc, err := os.StartProcess("/usr/bin/login", []string{"login", "-fpl", me.Username}, &pa)
+	proc, err := os.StartProcess("/usr/bin/env", []string{"/usr/bin/env", "zsh"}, pa)
+	if err != nil {
+		proc, err = os.StartProcess("/usr/bin/env", []string{"/usr/bin/env", "bash"}, pa)
+		if err != nil {
+			panic(err)
+		}
+	}
+	_, err = proc.Wait()
 	if err != nil {
 		panic(err)
 	}
-
-	//// Wait until user exits the shell
-	state, err := proc.Wait()
-	if err != nil {
-		panic(err)
-	}
-
-	//// Keep on keepin' on.
-	fmt.Printf("<< Exited shell: %s\n", state.String())
+	fmt.Print(">> Exit shell\n")
 }
 
 func genPackageYml() error {
